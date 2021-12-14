48f5f82dd7ae902dce251f63c09bdc533f942ff7
Li_Harden <68153902+s07362022@users.noreply.github.com>
2021-12-15 03:20:03 +0800

    Create file_cut_to_img.py

---
+++ file_cut_to_img.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+import math
+import datetime
+import glob
+
+import cv2
+from pytube import YouTube
+class FrameExtractor():
+    '''
+    Class used for extracting frames from a video file.
+    '''
+    def __init__(self, video_path):
+        self.video_path = video_path
+        self.vid_cap = cv2.VideoCapture(video_path)
+        self.n_frames = int(self.vid_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.fps = int(self.vid_cap.get(cv2.CAP_PROP_FPS))
+        
+    def get_video_duration(self):
+        duration = self.n_frames/self.fps
+        print(f'Duration: {datetime.timedelta(seconds=duration)}')
+        
+    def get_n_images(self, every_x_frame):
+        n_images = math.floor(self.n_frames / every_x_frame) + 1
+        print(f'Extracting every {every_x_frame} (nd/rd/th) frame would result in {n_images} images.')
+        
+    def extract_frames(self, every_x_frame, img_name, dest_path=None, img_ext = '.png'):
+        if not self.vid_cap.isOpened():
+            self.vid_cap = cv2.VideoCapture(self.video_path)
+        
+        if dest_path is None:
+            dest_path = os.getcwd()
+        else:
+            if not os.path.isdir(dest_path):
+                os.mkdir(dest_path)
+                print(f'Created the following directory: {dest_path}')
+        
+        frame_cnt = 0
+        img_cnt = 0
+        
+        while self.vid_cap.isOpened():
+            success,image = self.vid_cap.read() 
+            
+            if not success:
+                break
+            
+            if frame_cnt % every_x_frame == 0:
+                img_path = os.path.join(dest_path, ''.join([img_name, '_', str(img_cnt), img_ext]))
+                cv2.imwrite(img_path, image) 
+                print("OK") 
+                img_cnt += 1
+                
+            frame_cnt += 1
+        
+        self.vid_cap.release()
+        cv2.destroyAllWindows()
+
+new_filename = 'smoke.mp4'
+train_fe = FrameExtractor(new_filename)
+train_fe.get_video_duration()
+train_skip_frame_rate = 56
+train_fe.get_n_images(every_x_frame=train_skip_frame_rate)
+
+train_fe.extract_frames(every_x_frame=56,
+                        img_name='smoke',
+                        dest_path='E:\\workspace\\project_\\file_smoke\\')
